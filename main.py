89a4d178100c5cd6e5223b72ff1b5c68a4d2fd74
Jan Spacek <jan.spacek12@gmail.com>
2019-01-21 00:36:58 +0100

    add gcp yaml

--- main.py
+++ main.py
@@ -7,12 +7,11 @@ app = Quart(__name__)
 async def hello():
     return 'hello'
 
-"""
 @app.websocket('/ws')
 async def ws():
     while True:
         await websocket.send('hello')
-"""
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
 # [END gae_python37_app]
